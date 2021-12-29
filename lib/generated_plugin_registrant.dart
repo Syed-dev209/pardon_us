c31a03fecdd1f887eef51be98807d976e6f60cd9
Syed-dev209 <60479224+Syed-dev209@users.noreply.github.com>
2021-12-30 01:24:48 +0500

    Searching, live url integrated and android release signin configs

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,9 +2,9 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:agora_rtc_engine/agora_rtc_engine_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:connectivity_for_web/connectivity_for_web.dart';
 import 'package:file_picker/src/file_picker_web.dart';
@@ -19,7 +19,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  AgoraRtcEngineWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   ConnectivityPlugin.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
